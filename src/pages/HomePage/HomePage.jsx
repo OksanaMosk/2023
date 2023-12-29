a40966e0202d585374963a7d4d45fd5e9487f5db
OksanaMosk <oks-m3@ukr.net>
2023-12-29 20:21:51 +0200

    0

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,20 +1,20 @@
-import { useLocation } from 'react-router-dom';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import Loader from 'components/Loader/Loader';
+// import { useLocation } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
+// import Loader from 'components/Loader/Loader';
 
-import photo1 from 'images/icons8-phonebook-96.png';
-import photo2 from 'images/icons8-add-a-new-contact-on-modern-cell-phone-96.png';
+// import photo1 from 'images/icons8-phonebook-96.png';
+// import photo2 from 'images/icons8-add-a-new-contact-on-modern-cell-phone-96.png';
 
 import css from './HomePage.module.css';
 
 const HomePage = () => {
-  const isLoadingAuth = useSelector(state => state.auth.isLoadingAuth);
-  const location = useLocation();
+  // const isLoadingAuth = useSelector(state => state.auth.isLoadingAuth);
+  // const location = useLocation();
 
   return (
     <div className={css.home}>
-      {isLoadingAuth ? (
+      {/* {isLoadingAuth ? (
         <Loader />
       ) : (
         <>
@@ -33,7 +33,7 @@ const HomePage = () => {
             <img src={photo1} alt="{photo}" width={170} height={170}></img>
           </NavLink>
         </>
-      )}
+      )} */}
     </div>
   );
 };
