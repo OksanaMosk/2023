3c90384b2c20ddcabc7113a093af94d3a4a5a795
OksanaMosk <oks-m3@ukr.net>
2024-02-12 23:14:39 +0200

    1

--- src/components/CarouselHome/CarouselHome.jsx
+++ src/components/CarouselHome/CarouselHome.jsx
@@ -1,102 +1,70 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
 
-import '../CarouselHome/CarouselHome';
-import photo1 from '../imagesHome/01.png';
-import photo2 from '../imagesHome/02.jpg';
-import photo3 from '../imagesHome/03.jpg';
-import photo4 from '../imagesHome/04.jpg';
-import photo5 from '../imagesHome/05.jpg';
-import photo6 from '../imagesHome/06.jpg';
-import photo7 from '../imagesHome/07.jpg';
-import photo8 from '../imagesHome/08.jpg';
-import photo9 from '../imagesHome/09.jpg';
-import photo10 from '../imagesHome/10.jpg';
-import photo11 from '../imagesHome/11.jpg';
-import photo12 from '../imagesHome/12.jpg';
-import photo13 from '../imagesHome/13.jpg';
-import photo14 from '../imagesHome/14.jpg';
+const photosForDesktop = [
+  import('../imagesHome/forDesktop/01.png'),
+  ...[...Array(13).keys()].map(i =>
+    import(`../imagesHome/forDesktop/${String(i + 2).padStart(2, '0')}.jpg`)
+  ),
+];
+
+const photosForMobile = [
+  ...[...Array(12).keys()].map(it =>
+    import(`../imagesHome/forMobile/${String(it + 1)}.jpg`)
+  ),
+];
+console.log('photosForMobile: ', photosForMobile);
 
 export const CarouselHome = () => {
   const [imagesLoaded, setImagesLoaded] = useState(false);
+  const [images, setImages] = useState([]);
 
   useEffect(() => {
     const fetchImages = async () => {
+      const loadedModulesForDesktop = await Promise.all(photosForDesktop);
+      const loadedModulesForMobile = await Promise.all(photosForMobile);
+
+      const imagesMobile = loadedModulesForDesktop.map(module => ({
+        originalMobile: module.default,
+        originalClass: 'full-width-image',
+      }));
+
+      const imagesDesktop = loadedModulesForMobile.map(module => ({
+        originalDesktop: module.default,
+        originalClass: 'full-width-image',
+      }));
+
+      setImages([...imagesDesktop, ...imagesMobile]);
       setImagesLoaded(true);
     };
 
     fetchImages();
   }, []);
 
-  const images = [
-    {
-      original: photo1,
-      originalClass: 'full-width-image',
-    },
-    {
-      original: photo2,
-      originalClass: 'full-width-image',
-    },
-    {
-      original: photo3,
-      originalClass: 'full-width-image',
-    },
-    {
-      original: photo4,
-      originalClass: 'full-width-image',
-    },
-    {
-      original: photo5,
-      originalClass: 'full-width-image',
-    },
-    {
-      original: photo6,
-      originalClass: 'full-width-image',
-    },
-    {
-      original: photo7,
-      originalClass: 'full-width-image',
-    },
-    {
-      original: photo8,
-      originalClass: 'full-width-image',
-    },
-    {
-      original: photo9,
-      originalClass: 'full-width-image',
-    },
-    {
-      original: photo10,
-      originalClass: 'full-width-image',
-    },
-    {
-      original: photo11,
-      originalClass: 'full-width-image',
-    },
-    {
-      original: photo12,
-      originalClass: 'full-width-image',
-    },
-    {
-      original: photo13,
-      originalClass: 'full-width-image',
-    },
-    {
-      original: photo14,
-      originalClass: 'full-width-image',
-    },
-  ];
-
   const customImageRenderer = item => (
     <div className={`image-gallery-image ${item.originalClass || ''}`}>
-      <img
-        src={item.original}
-        alt={item.originalAlt}
-        className="full-width-image"
-        style={{ maxWidth: '100%', height: 'auto', objectFit: 'cover' }}
-      />
+      <picture>
+        <source
+          srcSet={`${item.originalDesktop} 100%`}
+          media="(min-width:1158px)"
+        />
+        <source
+          srcSet={`${item.originalMobile} 768w`}
+          media="(min-width:768px)"
+        />
+        <source
+          srcSet={`${item.originalMobile} 768w`}
+          media="(max-width:767px)"
+        />
+        <img
+          srcSet={item.originalDesktop}
+          alt={item.originalAlt}
+          className="full-width-image"
+          style={{ width: '100%', height: 'auto', objectFit: 'cover' }}
+          type="image"
+        />
+      </picture>
     </div>
   );
 
