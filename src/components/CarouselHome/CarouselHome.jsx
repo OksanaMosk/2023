4248063fff61287e8858f2705e12b47d20515164
OksanaMosk <oks-m3@ukr.net>
2024-02-28 23:43:09 +0200

    1

--- src/components/CarouselHome/CarouselHome.jsx
+++ src/components/CarouselHome/CarouselHome.jsx
@@ -2,84 +2,88 @@ import React, { useState, useEffect } from 'react';
 import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
 
-const photosForDesktop = [
-  import('../imagesHome/forDesktop/01.png'),
-  ...[...Array(13).keys()].map(i =>
-    import(`../imagesHome/forDesktop/${String(i + 2).padStart(2, '0')}.jpg`)
-  ),
-];
-console.log('photosForDesktop: ', photosForDesktop);
-
-const photosForMobile = [
-  ...[...Array(12).keys()].map(it =>
-    import(`../imagesHome/forMobile/${String(it + 1)}.jpg`)
-  ),
-];
-console.log('photosForMobile: ', photosForMobile);
-
 export const CarouselHome = () => {
   const [imagesLoaded, setImagesLoaded] = useState(false);
-  const [images, setImages] = useState([]);
+  const [isMobile, setIsMobile] = useState(false);
+  const [imagesDesktop, setImagesDesktop] = useState([]);
+  const [imagesMobile, setImagesMobile] = useState([]);
 
   useEffect(() => {
     const fetchImages = async () => {
-      const loadedModulesForDesktop = await Promise.all(photosForDesktop);
-      const loadedModulesForMobile = await Promise.all(photosForMobile);
+      const photosForDesktop = await Promise.all(
+        [...Array(14).keys()].map(i =>
+          import(
+            `../imagesHome/forDesktop/${String(i + 1).padStart(2, '0')}.jpg`
+          )
+        )
+      );
 
-      const imagesMobile = loadedModulesForDesktop.map(module => ({
+      const photosForMobile = await Promise.all(
+        [...Array(14).keys()].map(i =>
+          import(
+            `../imagesHome/forMobile/${String(i + 1).padStart(2, '0')}.jpg`
+          )
+        )
+      );
+
+      const imagesMobile = photosForMobile.map(module => ({
         originalMobile: module.default,
         originalClass: 'full-width-image',
       }));
 
-      const imagesDesktop = loadedModulesForMobile.map(module => ({
+      const imagesDesktop = photosForDesktop.map(module => ({
         originalDesktop: module.default,
-        originalClass: 'full-width-image',
+        originalClass: 'full-width-img',
       }));
 
-      setImages([...imagesDesktop, ...imagesMobile]);
+      setImagesDesktop(imagesDesktop);
+      setImagesMobile(imagesMobile);
+
+      const isMobileDevice = window.innerWidth <= 768;
+      setIsMobile(isMobileDevice);
+
       setImagesLoaded(true);
     };
 
     fetchImages();
   }, []);
 
-  const customImageRenderer = item => (
-    <div className={`image-gallery-image ${item.originalClass || ''}`}>
-      <picture>
-        <source
-          srcSet={`${item.originalDesktop} 100%`}
-          media="(min-width:1440px)"
-        />
-        <source
-          srcSet={`${item.originalMobile} 340w`}
-          media="(max-width:1439px)"
-        />
-        <source
-          srcSet={`${item.originalMobile} 380w`}
-          media="(max-width:767px)"
-        />
-        <img
-          srcSet={item.originalDesktop}
-          alt={item.originalAlt}
-          className="full-width-image"
-          style={{ width: '100%', height: 'auto', objectFit: 'cover' }}
-          type="image"
-        />
-      </picture>
-    </div>
-  );
+  const customImageRenderer = item =>
+    item.originalDesktop !== null && (
+      <div className={`image-gallery-image ${item.originalClass || ''}`}>
+        <picture>
+          <source
+            srcSet={`${item.originalDesktop} 100%`}
+            media="(min-width:1440px)"
+          />
+          <img
+            srcSet={isMobile ? item.originalMobile : item.originalDesktop}
+            alt={item.originalAlt}
+            className="full-width-image"
+            style={{
+              width: '100%',
+              height: isMobile ? '300px' : 'auto',
+              objectFit: 'cover',
+            }}
+            type="image"
+          />
+        </picture>
+      </div>
+    );
 
   return (
     <>
       {imagesLoaded ? (
-        <ImageGallery
-          items={images}
-          autoPlay={true}
-          disableArrowKeys={true}
-          renderItem={customImageRenderer}
-          showFullscreenButton={false}
-          slideDuration={1700}
-        />
+        <div style={{ height: isMobile ? '800px' : '100%' }}>
+          <ImageGallery
+            items={isMobile ? imagesMobile : imagesDesktop}
+            autoPlay={true}
+            disableArrowKeys={true}
+            renderItem={customImageRenderer}
+            showFullscreenButton={false}
+            slideDuration={1700}
+          />
+        </div>
       ) : (
         <p>Is loading...</p>
       )}
